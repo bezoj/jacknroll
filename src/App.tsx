6ef66a170ba221e8d03f93f0b5aac11d68dcdf1d
bezoj <jernej.bezjak9@gmail.com>
2024-06-05 19:28:54 +0200

    PT-3: Added temporary custom colors and fonts into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="bg-secondary">
+      <h1 className="text-3xl font-body font-normal">Hello world!</h1>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Roboto']
+      },
+      colors: {
+        'primary': '#0a0a0a',
+        'secondary': '#fafaf9'
+      }
+    },
   },
   plugins: [],
 }
