e42a9d603500ea7424c52633e8b58ca6e5b80ca1
bezoj <jernej.bezjak9@gmail.com>
2025-03-06 14:27:34 +0100

    PT-11: Small changes to app

--- src/components/containers/GalleryContainer.tsx
+++ src/components/containers/GalleryContainer.tsx
@@ -1,6 +1,6 @@
 export function GalleryContainer() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div>
         <img
           className="h-auto max-w-full"
